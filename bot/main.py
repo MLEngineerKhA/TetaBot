eb4748287266b4863a3d56b8675838b7f2100ae3
MLEngineerKhA <83141721+MLEngineerKhA@users.noreply.github.com>
2021-06-26 14:40:59 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -8,7 +8,7 @@ token = os.getenv("DISCORD_BOT_TOKEN")
 
 async def on_ready():
     activity = discord.Game(name="Netflix", type=1)
-    await bot.change_presence(status=discord.Status.idle, activity=activity)
+    await client.change_presence(status=discord.Status.idle, activity=activity)
     print("I am online")
 
 client.run(token)
