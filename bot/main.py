c003c37aaa82c504e98c90215f25d083086e21a7
MLEngineerKhA <83141721+MLEngineerKhA@users.noreply.github.com>
2021-06-26 15:02:06 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -7,7 +7,7 @@ token = os.getenv("DISCORD_BOT_TOKEN")
 
 
 async def on_ready():
-    await client.change_presence(activity=discord.Game('Sea of Thieves'))
+    await client.change_presence(activity=discord.Streaming(name='Sea of Thieves', url='https://www.twitch.tv/your_channel_here'))
     print("I am online")
 
 client.run(token)
