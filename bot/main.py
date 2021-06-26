d61b042067e2468b26c29910f12485f75ca0a62c
MLEngineerKhA <83141721+MLEngineerKhA@users.noreply.github.com>
2021-06-26 15:09:34 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -5,7 +5,7 @@ import os
 client = commands.Bot(command_prefix="/")
 token = os.getenv("DISCORD_BOT_TOKEN")
 
-
+@bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Game("Текст игры"))
     print("I am online")
